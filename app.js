eb3c8446abf762d739e2b500b5548e5369b738f5
Korchev <Korchev94@gmail.com>
2021-01-29 21:27:53 +0000

    desing refactoring

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
+const API_KEY = "4e500205a616505955675518272633a4"
 const city = document.querySelector(".city")
 const getWeatherBtn = document.querySelector(".get-weather")
-const API_KEY = "4e500205a616505955675518272633a4"
 const weatherText = document.querySelector(".weather-text")
 const app = document.querySelector(".main")
 const body = document.querySelector("body")
-
+const hourly = document.querySelector(".hourly")
 // Function to capitalise the 1st letter of the fetched weather description
 
 const capitalise = (str) => {
@@ -16,47 +16,44 @@ const capitalise = (str) => {
   return str
 }
 
+// Checks if the temperature is above 15° and adds the correct class to change BG
+const changeBackground = (data) => {
+  const temp = data.main.temp
+  const typeOfWeather = data.weather[0].main
+  switch (typeOfWeather) {
+    case "Clouds":
+      body.style.backgroundImage = "url('clouds.jpg')"
+      return
+    case "Clear":
+      body.style.backgroundImage = "url('sunny.jpg'"
+      return
+    case "Rain":
+      body.style.backgroundImage = " url('rain.jpg')"
+      return
+    case "Mist":
+      body.style.backgroundImage = "url('mist.jpg')"
+      return
+  }
+}
+
 const clickHandler = async (e) => {
   e.preventDefault()
-
   // Fetching data from API
-
   try {
-    const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&units=metric&appid=${API_KEY}`
-    )
-    let weather = await response.json()
-    console.log(weather)
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city.value}&units=metric&appid=${API_KEY}`)
+    let data = await response.json()
     city.value = ""
-    let description = capitalise(weather.weather[0].description)
-
-    // Template string ( Using Prettier to format and the formatting of template literals is terrible, not sure how to fix it without disabling which I don't wanna do! )
-
-    weatherText.innerHTML = `<div class="text-center">
-    <h1>${
-      weather.name
-    } </h1> <img class="small" src="https://openweathermap.org/img/wn/${
-      weather.weather[0].icon
-    }@2x.png"> 
-    <h5>${description}</h5>
-    <h2>${weather.main.temp.toFixed(
-      1
-    )}° feels like ${weather.main.feels_like.toFixed(1)}°</h2></div>
+    let description = capitalise(data.weather[0].description)
+    console.log(data)
+    weatherText.innerHTML = `
+    <div class="text-center">
+    <h1 class="display-4">${data.name}</h1> <img class="small" src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png"> 
+    <h5 class="display-6">${description}</h5>
+    <h2 class="display-5">${data.main.temp.toFixed(1)}° feels like ${data.main.feels_like.toFixed(1)}°</h2></div>
       `
-
-    // Conditions to change background
-
-    if (weather.main.temp < 15 && body.classList.contains("sun")) {
-      body.classList.replace("sun", "cold")
-    } else if (weather.main.temp <= 15 && !body.classList.contains("sun")) {
-      body.classList.add("cold")
-    } else if (weather.main.temp > 15 && body.classList.contains("cold")) {
-      body.classList.replace("cold", "sun")
-    } else if (weather.main.temp >= 15 && !body.classList.contains("cold")) {
-      body.classList.add("sun")
-    }
+    changeBackground(data)
   } catch (err) {
-    alert(err)
+    alert("Something went wrong! Please make sure you are typing the correct city name.")
   }
 }
 
